4bff02a8538f2f2a9fcd6402bd0e7451ec3110fd
bradgnar <bradgnar@gmail.com>
2014-02-20 15:25:08 -0500

    The CLI will take 1 - 3 arguments and then create a schema for you, none needs to be supplied

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ cli_csv2mongo
 =============
 
 This will be a command line interface that will take a csv file from somewhere in the system and load it into mongodb as json objects
+
+You can run the program with the command
+
+	node csv2mongo
+
+It will need at least one, but most likely 3 arguments.
+	
+	ARGUMENTS:
+		1) The first argument should be the full file path for the file you want to read in -NOTE: it must be a csv file.
+		2) The second argument is the db name for file.  If you don't enter this argument it will default to csv2mongo_db.
+		3) The third argument is the name of the collection. This should be singular, but note that when you look up the collection it will be changed to the plural form of whatever you wrote.  This is because csv2mongo uses mongoose and mongoose automatically pluralizes the "model" name.  If you do not enter this argument it will automatically be defaulted to csv2mongo_item, and will show up in the db as a collection named csv2mongo_items.
+
+
--- csv2mongo.js
+++ csv2mongo.js
@@ -16,8 +16,12 @@ if (userArguments.length > 3) {
 settings = {
 	file_to_read: userArguments[0],
 	db_name: userArguments[1] || 'csv2mongo_db',
-	collection_name: userArguments[2] || 'csv2mongo_item'
+	collection_name: userArguments[2] || 'csv2mongo_item',
+	schema: undefined
 };
+
+//After getting the basic settings the user will be prompted to enter in a schema
+// if the user does not enter in a schema the program will try to make a default schema
 
 
 
@@ -25,29 +29,8 @@ var Mongo = require("mongodb"),
 	csv = require("csv"),
 	fs = require("fs"),
 	mongoose = require("mongoose"),
-	MongoClient = Mongo.MongoClient;
-settings.schema = mongoose.Schema({
-		Column: String,
-		D8te: String,
-		Time: String,
-		Latitude: String,
-		Longitude: String,
-		WLDepth: String,
-		Speed: String,
-		Draft: String,
-		Temp: String,
-		Vessel: String,
-		Prev: String,
-		Next: String,
-		TimeBefore: String,
-		TimeAfter: String,
-		Pred: String,
-		DTBefore: String,
-		DTAfter: String,
-		DTCurrent: String,
-		Loc: String
-	}),
-	collection = mongoose.model(settings.collection_name, settings.schema);
+	MongoClient = Mongo.MongoClient,
+	collection;// = mongoose.model(settings.collection_name, settings.schema);
 
 	mongoose.connect('mongodb://localhost/' + settings.db_name);
 	var db = mongoose.connection
@@ -55,14 +38,24 @@ settings.schema = mongoose.Schema({
 	db.once('open', function callback(){
 		console.log('connection open');
 
-		csv().from.stream(fs.createReadStream(settings.file_to_read), {columns:true})
-			.transform(function(row){
-				//console.log(row);
+		csv().from.stream(fs.createReadStream(settings.file_to_read), {columns:true})			
+			.transform(function(row){				
 				return row;
 			})
+			.once('record', function(row, index){				
+				if(!settings.schema){
+					console.log('creating schema');
+
+					settings.schema = {};					
+					for(k in row){
+					 	settings.schema[k] = String;
+					}
+					collection = mongoose.model(settings.collection_name, settings.schema);
+				}
+				
+
+			})
 			.on('record', function(row, index){
-
-
 
 				var newItem = new collection(row);
 				newItem.save(function(err, newItem){
@@ -70,19 +63,18 @@ settings.schema = mongoose.Schema({
 						console.log(err.message);
 						return
 					}
-					console.log('item saved');
-				})
+					//console.log('item saved');
+				});
 				
 			})
 			.on('end', function(){
-
 				console.log('end hit ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;');
 
 				//this is just to check to see if the Bath data items are getting read in correctly
 				collection.find({}, function(err, dataPiece){
 					if(err) {console.log(err.message); return;}
-					console.log('ITEM FOUND');
-					console.log(dataPiece);
+					// console.log('ITEM FOUND');
+					// console.log(dataPiece);
 				});
 			});
 	});
