ba68ab6fd1d857e2a9cf851c3afd464bfa4ebb35
bradgnar <bradgnar@gmail.com>
2014-02-20 14:29:01 -0500

    Making the cli more usable for other people

--- csv2mongo.js
+++ csv2mongo.js
@@ -1,27 +1,32 @@
 #!/usr/bin/env node
+
+//this is the base setup for the program.  It first reads in the arguments from the user and if not supplied gives
+//default namings
 console.log('csv2mongo has started');
 var userArguments = process.argv.slice(2),
-	db_name, file_to_read; 
+	settings;
 
-if (userArguments.length > 2) {
-    throw new Error('Max two argument may be specified (the full file directory that you want to load in the db)');
-}else if(userArguments.length === 2){
-	db_name = userArguments[0];
-	file_to_read = userArguments[1];
-}else if (userArguments.length === 1) {
-	db_name = 'csv2mongo_db';
-	file_to_read = userArguments[0];
-}else{
+
+if (userArguments.length > 3) {
+    throw new Error('Max three arguments may be specified (the full file directory that you want to load in the db)');
+}else if(userArguments.length <= 0){
 	throw new Error('There needs to be at least a file path specified');
 }
+
+settings = {
+	file_to_read: userArguments[0],
+	db_name: userArguments[1] || 'csv2mongo_db',
+	collection_name: userArguments[2] || 'csv2mongo_item'
+};
+
 
 
 var Mongo = require("mongodb"),
 	csv = require("csv"),
 	fs = require("fs"),
 	mongoose = require("mongoose"),
-	MongoClient = Mongo.MongoClient,
-	bathSchema = mongoose.Schema({
+	MongoClient = Mongo.MongoClient;
+settings.schema = mongoose.Schema({
 		Column: String,
 		D8te: String,
 		Time: String,
@@ -42,22 +47,24 @@ var Mongo = require("mongodb"),
 		DTCurrent: String,
 		Loc: String
 	}),
-	BathDataItem = mongoose.model('BathDataItem', bathSchema);
+	collection = mongoose.model(settings.collection_name, settings.schema);
 
-	mongoose.connect('mongodb://localhost/' + db_name);
+	mongoose.connect('mongodb://localhost/' + settings.db_name);
 	var db = mongoose.connection
 	db.on('error', console.error.bind(console, 'connection error'));
 	db.once('open', function callback(){
 		console.log('connection open');
 
-		csv().from.stream(fs.createReadStream(file_to_read), {columns:true})
+		csv().from.stream(fs.createReadStream(settings.file_to_read), {columns:true})
 			.transform(function(row){
 				//console.log(row);
 				return row;
 			})
 			.on('record', function(row, index){
 
-				var newItem = new BathDataItem(row);
+
+
+				var newItem = new collection(row);
 				newItem.save(function(err, newItem){
 					if(err) {
 						console.log(err.message);
@@ -65,21 +72,14 @@ var Mongo = require("mongodb"),
 					}
 					console.log('item saved');
 				})
-				// var query = {Column: row.Column};
-				// row.Vessel = "zzzzzzzzzzzzzzzzzzzzzzzzzzz";
-				// console.log(row);
-				// BathDataItem.update(query, row, {upsert: true},function(err, numEff){
-				// 	if(err) console.log(err.message);
-
-				// 	console.log(numEff);
-				// });
+				
 			})
 			.on('end', function(){
 
 				console.log('end hit ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;');
 
 				//this is just to check to see if the Bath data items are getting read in correctly
-				BathDataItem.find({}, function(err, dataPiece){
+				collection.find({}, function(err, dataPiece){
 					if(err) {console.log(err.message); return;}
 					console.log('ITEM FOUND');
 					console.log(dataPiece);
